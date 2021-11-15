026907b8a80026d592fbcff8b6de5944aa907e3f
YihuanZhou <1209201927@qq.com>
2021-11-16 00:31:42 +0800

    LearnedSkiplist based on sub tree version1
    每段区间左闭右开

--- include/skiplist.hpp
+++ include/skiplist.hpp
@@ -11,11 +11,32 @@
 #include <sstream>
 #include <string>
 #include <map>
+#include <stack>
+#include <cstring>
 // #include "./fileOperation.hpp"
+
+typedef uint8_t bitmap_t;
+
+#define BITMAP_WIDTH (sizeof(bitmap_t) * 8)
+#define BITMAP_SIZE(num_items) (((num_items) + BITMAP_WIDTH - 1) / BITMAP_WIDTH)
+#define BITMAP_GET(bitmap, pos) (((bitmap)[(pos) / BITMAP_WIDTH] >> ((pos) % BITMAP_WIDTH)) & 1)
+#define BITMAP_SET(bitmap, pos) ((bitmap)[(pos) / BITMAP_WIDTH] |= 1 << ((pos) % BITMAP_WIDTH))
+#define BITMAP_CLEAR(bitmap, pos) ((bitmap)[(pos) / BITMAP_WIDTH] &= ~bitmap_t(1 << ((pos) % BITMAP_WIDTH)))
+#define BITMAP_NEXT_1(bitmap_item) __builtin_ctz((bitmap_item))
 
 #define p 0.5
 #define UNINT_MAX 0xffffffff
 #define Epslion 1e-8
+
+
+// runtime assert
+#define RT_ASSERT(expr) \
+{ \
+    if (!(expr)) { \
+        fprintf(stderr, "RT_ASSERT Error at %s:%d, `%s`\n", __FILE__, __LINE__, #expr); \
+        exit(0); \
+    } \
+}
 
 using namespace std;
 
@@ -243,81 +264,263 @@ typedef struct snode{
 	unsigned int level;
 }snode;
 
-class Segment_pt :public Segment{
+class skiplist;
+
+class Segment_pt{
     public:
-        vector<Segment_pt*> forward;
-		//Segment_pt** forward;
-        unsigned level;
-        int node_size;
-		//todo
-		node* nodes;
-        // vector<node> nodes;
-		// unsigned int fisrt_index;
+		struct subtree;
+		struct Item{
+				union {
+					node data;
+					subtree* child;
+				}comp;
+		};
+		struct subtree
+		{
+			int is_two = 0; // is special node for only two keys
+			int build_size = 0; // tree size (include sub nodes) when node created
+			int size = 0; // current tree size (include sub nodes)
+			int fixed = 0; // fixed node will not trigger rebuild
+			int num_inserts = 0, num_insert_to_data = 0;
+			int child_ptr = 0;
+			int num_items = 0; // size of items
+			double slope = 0,intercept = 0;
+			unsigned int start = UNINT_MAX,stop=UNINT_MAX;
+			Item* items = nullptr;
+			bitmap_t* none_bitmap = nullptr; // 1 means None, 0 means Data or Child
+			bitmap_t* child_bitmap = nullptr; // 1 means Child. will always be 0 when none_bitmap is 1
+			const double BUILD_LR_REMAIN = 0;
+		};
+
+		typedef struct {
+            int begin;
+            int end;
+            int lvl; // top lvl = 1
+            subtree* n;
+		} Seg;
+
+        vector<Segment_pt*> forward;//Segment_pt** forward;
+        int level;
+        subtree* DataArray;
 
         Segment_pt();
-		Segment_pt(int level,unsigned int strt,unsigned int stp,double slp,double interc):Segment(strt,stp,slp,interc){
-			// cerr<<"????????"<<endl;
-			// this->fisrt_index = stidx;
+		Segment_pt(int level,subtree* n){
             this->level = level;
-             vector<Segment_pt*> new_seg(level+1);
-			//this->forward = (Segment_pt**)malloc(sizeof(Segment_pt*)*(level+1));
-             this->forward = new_seg;
+            vector<Segment_pt*> new_seg(level+1);//this->forward = (Segment_pt**)malloc(sizeof(Segment_pt*)*(level+1));
+			this->forward = new_seg;
             for(int i=1;i<=level;i++){
                 this->forward[i] = this;
             }
-			// cerr<<"@@@@@"<<endl;
-        }
+			std::vector<subtree*> trees;
+            for (int _ = 0; _ < 1e3; _ ++) {
+                subtree* tr = build_tree_two(0, 0, 1, 0,0,0,nullptr);
+                trees.push_back(tr);
+            }
+            for (auto tr : trees) {
+                destroy_subtree(tr);
+            }
 
-        Segment_pt(int nodeSize,node* nodes,int level,unsigned int strt,unsigned int stp,double slp,double interc):Segment(strt,stp,slp,interc)
+			DataArray = n;
+        }
+		Segment_pt(int level){
+            this->level = level;
+            vector<Segment_pt*> new_seg(level+1);//this->forward = (Segment_pt**)malloc(sizeof(Segment_pt*)*(level+1));
+			this->forward = new_seg;
+            for(int i=1;i<=level;i++){
+                this->forward[i] = this;
+            }
+			if(level < 15){
+				std::vector<subtree*> trees;
+				for (int _ = 0; _ < 1e3; _ ++) {
+					subtree* tr = build_tree_two(0, 0, 1, 0,0,0,nullptr);
+					trees.push_back(tr);
+				}
+				for (auto tr : trees) {
+					destroy_subtree(tr);
+				}
+			}
+			DataArray = build_tree_none();
+        }
+		~Segment_pt(){
+			destroy_subtree(DataArray);
+			DataArray = NULL;
+			destory_pending();
+		}
+
+		std::stack<subtree*> pending_two;
+
+		std::allocator<subtree> subtree_allocator;
+		subtree* new_subtree(int n){
+			subtree* x = subtree_allocator.allocate(n);
+			if(x!=nullptr){
+				return x;
+			}
+			cerr<<"allocate failed"<<endl;
+			return nullptr;
+		}
+		void delete_subtree(subtree* x,int n){
+			subtree_allocator.deallocate(x,n);
+		}
+
+		void destroy_subtree(subtree* root);
+
+		void destory_pending()
 		{
-			// cerr<<"????????"<<endl;
-			// this->fisrt_index = stidx;
-            this->level = level;
-             vector<Segment_pt*> new_seg(level+1);
-             this->forward = new_seg;
-			//this->forward = (Segment_pt**)malloc(sizeof(Segment_pt*)*(level+1));
-            for(int i=1;i<=level;i++){
-                this->forward[i] = this;
-            }
-			this->node_size = nodeSize;
-			this->nodes = nodes;
-			// nodes.resize(input.size());
-			// nodes.assign(input.begin(),input.end());
-			// cerr<<"new finish"<<endl;
-        }
+			while (!pending_two.empty()) {
+				subtree* n = pending_two.top(); pending_two.pop();
 
-		void show(int w);
+				delete_items(n->items, n->num_items);
+				const int bitmap_size = BITMAP_SIZE(n->num_items);
+				delete_bitmap(n->none_bitmap, bitmap_size);
+				delete_bitmap(n->child_bitmap, bitmap_size);
+				delete_subtree(n, 1);
+			}
+		}
+
+		std::allocator<Item> item_allocator;
+		Item* new_items(int n)
+		{
+			Item* x = item_allocator.allocate(n);
+			// RT_ASSERT(p != NULL && p != (Item*)(-1));
+			return x;
+		}
+		void delete_items(Item* x, int n)
+		{
+			item_allocator.deallocate(x, n);
+		}
+
+		std::allocator<bitmap_t> bitmap_allocator;
+		bitmap_t* new_bitmap(int n)
+		{
+			bitmap_t* x = bitmap_allocator.allocate(n);
+			// RT_ASSERT(x != NULL && x != (bitmap_t*)(-1));
+			return x;
+		}
+		void delete_bitmap(bitmap_t* x, int n)
+		{
+			bitmap_allocator.deallocate(x, n);
+		}
+		
+		bool checkTree(subtree* n){
+			int shouldbe = n->size;
+            int real = 0;
+			int prt = 0;
+            for(int j = 0;j<n->num_items;j++){
+                if(!BITMAP_GET(n->none_bitmap,j)){
+                    if(BITMAP_GET(n->child_bitmap,j)){
+						if(checkTree(n->items[j].comp.child)){
+                        	real+=(n->items[j].comp.child->size);
+							prt++;
+						}
+						else
+							return false;
+                    }
+                    else{
+                        real++;
+                    }
+                }
+            }
+            if(shouldbe!=real){
+                cerr<<"rebuild "<<n<<"error"<<endl;
+				return false;
+            }
+			return true;
+		}
+
+		void insert_subtree(unsigned int key,int value,Segment_pt** Update,skiplist* list);
+		
+		std::pair<int,Segment_pt::Item*> find_subtree(unsigned int key);
+
+		std::pair<unsigned int,unsigned int> computeRange(int pos,subtree* x){
+			unsigned int start = 0,stop = 0;
+			start = (pos - x->intercept)/(x->slope * 1000.0)*1000.0;
+			stop = (pos+1 - x->intercept)/(x->slope * 1000.0)*1000.0;
+			return {start+x->start,stop+x->start};
+		}
+
+		inline int compute_gap_count(int size) {
+			if (size >= 1000000) return 1;
+			if (size >= 100000) return 2;
+			return 5;
+		}
+
+		inline int predict(subtree*root,unsigned int key){
+			// cerr<<root->slope<<","<<root->intercept<<endl;
+			double v = root->slope * (static_cast<long double>(key)-static_cast<long double>(root->start)) + root->intercept;
+			// if (std::isnan(v)){
+			// 	return root->num_items - 1;
+			// }
+			if(v > std::numeric_limits<int>::max() / 2) {
+            	return root->num_items - 1;
+			}
+			if(v < 0 || static_cast<int>(v)<0){
+				// cerr<<"prd:"<<v<<endl;
+				return 0;
+			}
+			return std::min(root->num_items - 1, static_cast<int>(v));
+		}
+
+		bool SplitSegment(Segment_pt** Update,skiplist* list);
+
+		/// build an empty tree
+		subtree* build_tree_none();
+
+		subtree* build_tree_two(unsigned int key1, int value1, unsigned int key2, int value2,unsigned int start=0,unsigned int stop=0,subtree* x = nullptr);
+
+		subtree* rebuild_tree(unsigned int *_keys,int* _values,int _size,unsigned int start,unsigned int stop);
+
+		void scan_and_destroy_subtree(subtree* root,unsigned int*keys,int* values, bool destory = true);
+
+		void showArray(subtree* n){
+			cerr<<"-";
+			for (int i = 0; i < n->num_items; i ++) {
+				if (BITMAP_GET(n->none_bitmap, i) == 0) {
+					if (BITMAP_GET(n->child_bitmap, i) == 0) {
+						cerr<<n->items[i].comp.data.key<<",";
+					} else {
+						showArray(n->items[i].comp.child);
+					}
+				}
+			}
+		}
+
+		// void show(int w);
 
 };
 
 class skiplist {
     public:
-        int level;
-        int size;
+        int skip_level;
+        int segCnt;
         Segment_pt *header;
         int MaxLevel;
 		int gamma;
 
         skiplist();
         skiplist(int MaxLevel,int gamma){
-            int i;
-            Segment_pt *header = new Segment_pt(MaxLevel,UNINT_MAX,UNINT_MAX,0,0);//(snode *)malloc(sizeof(struct snode));
+            Segment_pt *header = new Segment_pt(MaxLevel);//(snode *)malloc(sizeof(struct snode));
             this->header = header;
-            // for (i = 0; i <= MaxLevel; i++) {
-            //     header->forward[i] = this->header;
-            // }
-            this->level = 1;
-            this->size = 0;
+            this->skip_level = 1;
+            this->segCnt = 0;
             this->MaxLevel = MaxLevel;
 			this->gamma = gamma;
+			// Segment_pt* newSeg = new Segment_pt(this->RandLevel(),0,0,0,0);
+			// for(int i = newSeg->level;i>=1;i--){
+			// 	this->header->forward[i] = newSeg;
+			// 	newSeg->forward[i] = this->header;
+			// }
         }
 
 		void setup(vector<snode> input);
 
 		node* binarySearch(Segment_pt* x,unsigned int key);
-		node* Search(unsigned int key);
+		std::pair<int,Segment_pt::Item*> Search(unsigned int key);
+
+		void Insert(unsigned int key,int value);
 
 		void insert_static(vector<Segment_pt*> &Update,Segment* seg,unsigned int st,unsigned int ed,node* input,int size,int level);
+
+		void show();
 
         void ShowNodeDis();
         void Dump();
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
-#pragma once
 #include "../include/skiplist.hpp"
 #include <fstream>
 #include <string>
 #include <algorithm>
 #include <time.h>
 
-#define NUMBERDATA 5000000
-#define MaxL (int)(log(NUMBERDATA)/log(2))
+#define NUMBERDATA 50000
+#define MaxL 9//(int)(log(NUMBERDATA)/log(2))
 #define Gma 128
 #define Verify 0
 #define NOFINDDEBUG 0
@@ -15,75 +14,29 @@ using namespace std;
 
 int length;
 vector<snode> dataInput;
+vector<unsigned int> dataq0(NUMBERDATA);
 int MaxLevel = 0;
 vector<snode> nofind;
 
-unsigned int readFromCSV(vector<snode> &data)
-{
-    ifstream inFile("../static/exp_log2.csv", ios::in);
-    if (!inFile)
-    {
-        cout << "打开文件失败！" << endl;
-        exit(1);
-    }
-    srand((int)time(0));
-    vector<snode> data_x;
-    // int i = 0;
-    string line;
-    string field;
-    unsigned int res=0;
-    while (getline(inFile, line))//getline(inFile, line)表示按行读取CSV文件中的数据
-    {
-        string field1,field2;
-        istringstream sin(line); //将整行字符串line读入到字符串流sin中
-
-        getline(sin, field1, ','); //将字符串流sin中的字符读入到field字符串中，以逗号为分隔符 
-        // cout<<atoi(field.c_str())<<" ";//将刚刚读取的字符串转换成int
-
-        // getline(sin, field2, ','); //将字符串流sin中的字符读入到field字符串中，以逗号为分隔符 
-        // cout << atoi(field.c_str()) << " ";//将刚刚读取的字符串转换成int
-        // cout<<endl;
-        // i++;
-        snode x;
-        x.key = atoi(field1.c_str());
-        int NewLevel = 1;
-        int P = 50;
-        while(1){
-            int t = rand() % 101;
-            if(t<P)
-                NewLevel++;
-            else
-                break;
-        }
-        NewLevel =  min(NewLevel,MaxL);
-        x.level = NewLevel;
-        data_x.push_back(x);
-        res = max(res,x.level);
-    }
-    inFile.close();
-
-    data.assign(data_x.begin(),data_x.end());
-    return res;
-    // cout << "read " << i << "lines" << endl;
-    // cout << "读取数据完成" << endl;
-}
+extern int collsion;
+extern int rebuild_cnt;
+extern int split_cnt;
 
 void GetData()
 {
     int length = 0;
     srand((int)time(0));
     unsigned int *dataIoT;
-    std::ifstream inF("../../../datasets/iotdeleteZ.bin", std::ios::binary);
+    std::ifstream inF("../../iotShuffle.bin", std::ios::binary);
     inF.seekg(0, inF.end);
     length = inF.tellg();
     inF.seekg(0, inF.beg);
 
-    std::cerr << length << std::endl;
+    // std::cerr << length << std::endl;
     dataIoT = (unsigned int*)malloc(sizeof(unsigned int) * length);
 
-    inF.read(reinterpret_cast<char *>(dataIoT), sizeof(unsigned int) * length);
+    inF.read(reinterpret_cast<char *>(dataIoT), sizeof(unsigned int) * NUMBERDATA);
     inF.close();
-    vector<unsigned int> dataq0(length);
     int j=0;
     for(int i = 0;i<length;i++){
         if(dataIoT[i]!=0){
@@ -93,52 +46,73 @@ void GetData()
     }
     dataq0.resize(j);
     length = j;
+    free(dataIoT);
 
-    int bound = min(NUMBERDATA,length);
-    for(int i=0;i<bound;i++){
-        snode x;
-        x.key = dataq0[i];
-        int NewLevel = 1;
-        int P = 50;
-        while(1){
-            int t = rand() % 101;
-            if(t<P)
-                NewLevel++;
-            else
-                break;
-        }
-        NewLevel =  min(NewLevel,MaxL);
-        x.level = NewLevel;
-        dataInput.push_back(x);
-        MaxLevel = max(MaxLevel,NewLevel);
-    }
+    // int bound = min(NUMBERDATA,length);
+
+    // for(int i=0;i<bound;i++){
+    //     snode x;
+    //     x.key = dataq0[i];
+    //     int NewLevel = 1;
+    //     int P = 50;
+    //     while(1){
+    //         int t = rand() % 101;
+    //         if(t<P)
+    //             NewLevel++;
+    //         else
+    //             break;
+    //     }
+    //     NewLevel =  min(NewLevel,MaxL);
+    //     x.level = NewLevel;
+    //     dataInput.push_back(x);
+    //     MaxLevel = max(MaxLevel,NewLevel);
+    // }
 }
 
-void ExpSearch(skiplist* list){
+// void ExpSearch(skiplist* list){
+//     clock_t start,end;
+//     double sumTime = 0;
+//     int bound = NUMBERDATA;
+//     cerr<<"search bound"<<bound<<endl;
+//     int nofindcnt = 0;
+//     start = clock();
+//     for (int i = 0; i < bound; i++) {
+//         node* res = list->Search(dataInput[i].key);
+// #if NOFINDDEBUG
+//         if(!res || res->key!=dataInput[i].key){
+//             nofindcnt++;
+//             snode x;
+//             x.key = dataInput[i].key;
+//             x.level = i;
+//             nofind.push_back(x);
+//         }
+// #endif
+//     }
+//     end = clock();
+//     sumTime =(double(end-start)/CLOCKS_PER_SEC);
+// #if NOFINDDEBUG
+//     cerr<<"no find cnt:"<<nofindcnt<<endl;
+// #endif
+//     cerr<<"Search time = "<<sumTime<<"s"<<endl;  //输出时间（单位：ｓ）
+// }
+
+void ExpInsert(skiplist* list){
     clock_t start,end;
     double sumTime = 0;
-    int bound = NUMBERDATA;
-    cerr<<"search bound"<<bound<<endl;
-    int nofindcnt = 0;
     start = clock();
-    for (int i = 0; i < bound; i++) {
-        node* res = list->Search(dataInput[i].key);
-#if NOFINDDEBUG
-        if(!res || res->key!=dataInput[i].key){
-            nofindcnt++;
-            snode x;
-            x.key = dataInput[i].key;
-            x.level = i;
-            nofind.push_back(x);
+    for(int i = 0;i<NUMBERDATA;i++){
+        if(!dataq0[i]) continue;
+        list->Insert(dataq0[i],i);
+        std::pair<int,Segment_pt::Item*> res = list->Search(dataq0[i]);
+        if(!res.first || (res.second)->comp.data.value != i){
+            cerr<<"not find"<<endl;
         }
-#endif
     }
     end = clock();
     sumTime =(double(end-start)/CLOCKS_PER_SEC);
-#if NOFINDDEBUG
-    cerr<<"no find cnt:"<<nofindcnt<<endl;
-#endif
-    cerr<<"Search time = "<<sumTime<<"s"<<endl;  //输出时间（单位：ｓ）
+    cerr<<"Insert time = "<<sumTime<<"s"<<endl;  //输出时间（单位：ｓ）
+    cerr<<"collision:"<<collsion<<"\trebuild:"<<rebuild_cnt<<"\tsplit"<<split_cnt<<endl;
+    // list->show();
 }
 
 int main(){
@@ -148,13 +122,16 @@ int main(){
     cerr<<"length:"<<length<<endl;
     // cerr<<"Max Level:"<<MaxLevel<<endl;
     skiplist* list = new skiplist(MaxL,Gma);
-    list->setup(dataInput);
-    list->ShowNodeDis();
+
+    ExpInsert(list);
+
+    // list->setup(dataInput);
+    // list->ShowNodeDis();
 
     //show space size
-    list->ComputeSpace();
+    // list->ComputeSpace();
     //search test
-    ExpSearch(list);
+    // ExpSearch(list);
 
 #if Verify
     cerr<<"verify:"<<endl;
