c7ed3b0fb791564cc1a640fc7857ffc731ea2949
YihuanZhou <1209201927@qq.com>
2022-01-07 00:20:05 +0800

    add test code

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 #include <thread>
 
 #define MM 1000000
-#define NUMBERDATA (32*MM)
+#define NUMBERDATA (64*MM)
 #define PREINSERT 4*MM
 #define SkiplistMaxLevel 9//(int)(log(NUMBERDATA)/log(2))
-#define THREAD_NUMBER 32
+#define THREAD_NUMBER 16
 #define NOFINDDEBUG 0
 #define QUERY_ 0
 
+int key_dis = (NUMBERDATA-PREINSERT)/THREAD_NUMBER;
 unsigned int *dataq0 = new unsigned int[NUMBERDATA];
 skiplist<unsigned int,int> *list = new skiplist<unsigned int,int>(SkiplistMaxLevel,Gm);
 
@@ -35,13 +36,35 @@ void GetData(){
     std::shuffle(dataq0, dataq0+NUMBERDATA, g);
 }
 
+void test(const int id,const int bound_l,const int bound_r ){
+    for(int i = bound_l;i<bound_r;i++){
+        if(dataq0[i] == 0 || dataq0[i] == UNINT_MAX)
+            continue;
+        // cout<<i<<endl;
+        list->Add(dataq0[i],i);
+    }
+}
+
 int main(){
     GetData();
     srand((int)time(0));
     std::cout<<"NUMBERDATA:"<<NUMBERDATA<<std::endl;
+    std::vector<thread> threads;
+
     const auto start_time = std::chrono::steady_clock::now();
-    for(int i = 0;i<NUMBERDATA;i++){
+    for(int i = 0;i<PREINSERT;i++){
+        if(dataq0[i] == 0 || dataq0[i] == UNINT_MAX)
+            continue;
         list->Add(dataq0[i],i);
+    }
+    std::cout<<"PREINSERT finish"<<std::endl;
+    int kk = PREINSERT;
+    for(int idx=0; idx < THREAD_NUMBER; idx++){
+        threads.push_back(thread(test, idx,kk,kk+key_dis));
+        kk+=key_dis;
+    } 
+    for(int idx=0; idx <THREAD_NUMBER; idx++){
+        threads[idx].join();
     }
     const auto end_time = std::chrono::steady_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
