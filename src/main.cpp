5be6bcbd9d82ab93457a08156361f6be50d5d805
YihuanZhou <1209201927@qq.com>
2022-06-04 19:46:08 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/Conskiplist.hpp"
 #include <iostream>
 #include <thread>
+#include <set>
 
 #define PRFO 0
 #define PRFOINSERT 0
@@ -12,19 +13,20 @@
 
 #define MM 1000000
 #define NUMBERDATA (64*MM)
-#define SkiplistMaxLevel (int)(log(1500)/log(2))
+#define SkiplistMaxLevel (int)(log(3029)/log(2))//20//(int)(log(1500)/log(2))
 #define THREAD_NUMBER 32
 #define NOFINDDEBUG 0
-#define QUERY_TEST 1
-#define TEST_4 0
+#define QUERY_TEST 0
+#define WriteRatio (0.3)
 
-#if TEST_4
-int key_dis = (4*MM)/THREAD_NUMBER;
-#else
-int key_dis = (NUMBERDATA)/THREAD_NUMBER;
-#endif
-unsigned int *input_data = new unsigned int[NUMBERDATA];
-skiplist<unsigned int,int> *list = new skiplist<unsigned int,int>(SkiplistMaxLevel,Gm);
+
+int read_offset = NUMBERDATA * WriteRatio;
+int write_dis = (read_offset)/THREAD_NUMBER;
+int read_dis = (NUMBERDATA * (1 - WriteRatio)) / THREAD_NUMBER;
+char data_file[] = "/root/LSDataset/wiki/wiki64M73.txt";
+
+KeyType *input_data = new KeyType[NUMBERDATA];
+skiplist<KeyType,VaueType,ModelType> *list = new skiplist<KeyType,VaueType,ModelType>(SkiplistMaxLevel,Gm);
 
 long long partial_r[THREAD_NUMBER];
 long long MAX_DEPTH_array[THREAD_NUMBER];
@@ -34,19 +36,24 @@ long long collision_total[THREAD_NUMBER];
 long long insert_time_total[THREAD_NUMBER];
 char search_time[] = "./search_time.csv";
 char insert_time[] = "./insert_time.csv";
+char workload_time[] = "./workload73_time.csv";
 
 using namespace chrono;
 
-void GetData(){
-    //ycsb64M
-    char unique_dadta_file[] = "/home/yhzhou/datasets/ycsb64M.csv";
-    ifstream fp(unique_dadta_file);
-    string line;
-    for(int i =0;i<NUMBERDATA;i++){
-        getline(fp,line);
-        input_data[i] = atoi(line.c_str());  
+void GetDataFromText(char path[]){
+    // set<KeyType> unique;
+    ifstream myfile(path);
+    if (!myfile.is_open())
+	{
+		cout << "can not open this file" << endl;
+		exit(-1);
+	}
+    for(int i = 0;i<NUMBERDATA;i++){
+        myfile >> input_data[i];
+        int k;
+        myfile >> k;
     }
-    fp.close();
+    myfile.close();
     // std::random_device rd;
     // std::mt19937 g(rd());
     // std::shuffle(input_data, input_data+NUMBERDATA, g);
@@ -54,12 +61,12 @@ void GetData(){
 
 void test_insert(const int id,const int bound_l,const int bound_r ,int dijiduan){
     long long rebuild_cnt = 0;
-    int MaxDepth = 1;
+    // int MaxDepth = 1;
     int SplitCnt = 0;
     long long scan_on_rebuild_topk = 0;
     long long collision_ = 0;
     for(int i = bound_l;i<bound_r;i++){
-        if(input_data[i] == 0 || input_data[i] == UNINT_MAX)
+        if(input_data[i] == 0 || input_data[i] == KeyMax)
             continue;
         // list->Add(input_data[i],i,cnt);
         int cnt = 0;
@@ -78,10 +85,10 @@ void test_insert(const int id,const int bound_l,const int bound_r ,int dijiduan)
 }
 
 void test_query(const int id,const int bound_l,const int bound_r ){
-    std::pair<int,int> res;
+    std::pair<int,VaueType> res;
     int nofind = 0;
     for(int i = bound_l;i<bound_r;i++){
-        if(input_data[i] == 0 || input_data[i] == UNINT_MAX)
+        if(input_data[i] == 0 || input_data[i] == KeyMax)
             continue;
         // list->Lookup(input_data[i]);
         res = list->Lookup(input_data[i]);
@@ -93,19 +100,68 @@ void test_query(const int id,const int bound_l,const int bound_r ){
         std::cout<<"nofind:\t"<<nofind<<std::endl;
 }
 
+void WorkloadTest(int id){
+    int bound_l = id * (write_dis);
+    int bound_r = (id+1)*(write_dis);
+    // printf("range[%d,%d)\n",bound_l,bound_r);
+    int SplitCnt = 0;
+    for(int i = bound_l;i<bound_r;i++){
+        if(input_data[i] == 0 || input_data[i] == KeyMax)
+            continue;
+        // list->Add(input_data[i],i,cnt);
+        int cnt = 0;
+        long long scan = 0;
+        long long collision_cnt = 0;
+        list->Add(input_data[i],i,cnt,scan,SplitCnt,collision_cnt);
+    }
+    // std::cout<<"insert finish"<<std::endl;
+    bound_l = read_offset + id*(read_dis);
+    bound_r = read_offset + (id+1)*(read_dis);
+    for(int i = bound_l;i<bound_r;i++){
+        if(input_data[i] == 0 || input_data[i] == KeyMax)
+            continue;
+        auto res = list->Lookup(input_data[i]);
+    }
+}
+
+void WorkloadInserPart(int id){
+    int bound_l = id * (write_dis);
+    int bound_r = (id+1)*(write_dis);
+    // printf("range[%d,%d)\n",bound_l,bound_r);
+    int SplitCnt = 0;
+    for(int i = bound_l;i<bound_r;i++){
+        if(input_data[i] == 0 || input_data[i] == KeyMax)
+            continue;
+        // list->Add(input_data[i],i,cnt);
+        int cnt = 0;
+        long long scan = 0;
+        long long collision_cnt = 0;
+        list->Add(input_data[i],i,cnt,scan,SplitCnt,collision_cnt);
+    }
+}
+
+void WorkloadQueryPart(int id){
+    int bound_l = read_offset + id*(read_dis);
+    int bound_r = read_offset + (id+1)*(read_dis);
+    for(int i = bound_l;i<bound_r;i++){
+        if(input_data[i] == 0 || input_data[i] == KeyMax)
+            continue;
+        auto res = list->query(input_data[i]);
+    }
+}
+
 void Insert_Part(int &kk,int id){
-    std::vector<thread> threads;
     #if PRFOINSERT
-    string name = "test"+std::to_string(id)+".prof";
+    string name = "insert.prof";
     ProfilerStart(name.c_str());
     #endif
+    std::vector<thread> threads;
     for(int i = 0;i<THREAD_NUMBER;i++){
         partial_r[i] = 0;
     }
     const auto start_time = std::chrono::steady_clock::now();
     for(int idx=0; idx < THREAD_NUMBER; idx++){
-        threads.push_back(thread(test_insert, idx,kk,kk+key_dis,id));
-        kk+=key_dis;
+        threads.push_back(thread(WorkloadTest,idx));
     } 
     for(int idx=0; idx <THREAD_NUMBER; idx++){
         threads[idx].join();
@@ -115,25 +171,11 @@ void Insert_Part(int &kk,int id){
     #endif
     const auto end_time = std::chrono::steady_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-    long long cnt = 0;
-    long long split_cnt = 0;
-    long long time_scan = 0;
-    long long collision_cnt = 0;
-    for(int i = 0;i<THREAD_NUMBER;i++){
-        cnt+=partial_r[i];
-        time_scan+=scan_time_on_check[i];
-        // std::cout<<i<<" time on split/scan:"<<time_scan<<std::endl;
-        split_cnt+=split_total[i];
-        collision_cnt+=collision_total[i];
-    }
-    insert_time_total[id] = duration.count();
-    string outs = "insert time: "+ std::to_string( duration.count()) + "us\trebuild cnt: " + std::to_string(cnt)+ 
-        "\ttime on split/rebuild: " + std::to_string(time_scan) +"\tsplit cnt:"+std::to_string(split_cnt)+
-        "\tcollision cnt:"+std::to_string(collision_cnt)+"\t";
-    std::cout<<outs;
+    string outs = "operation time: "+ std::to_string( duration.count()) +"\t";
+    std::cout<<outs<<std::endl;
     list->ShowSegmentNumber(false);
     string k = std::to_string(duration.count())+"\n";
-    write_into_file(insert_time,k.c_str());
+    write_into_file(workload_time,k.c_str());
 }
 
 void Query_Part(int &st){
@@ -162,8 +204,8 @@ void Query_Part(int &st){
 }
 
 void RangeQuery_Test(){
-    vector<std::pair<unsigned int,int>> rg_res;
-    unsigned int st = 500,ed = 1480000;
+    vector<std::pair<KeyType,VaueType>> rg_res;
+    KeyType st = 500,ed = 1480000;
     list->Lookup(st,ed,rg_res);
     for(auto it:rg_res){
         std::cout<<it.first<<","<<it.second<<std::endl;
@@ -173,7 +215,7 @@ void RangeQuery_Test(){
 
 int main(){
     const auto start_time = std::chrono::steady_clock::now();
-    GetData();
+    GetDataFromText(data_file);
     srand((int)time(0));
     std::cout<<"NUMBERDATA:"<<NUMBERDATA<<"\tTHREAD_NUMBER:"<<THREAD_NUMBER<<std::endl;
     std::cout<<"SkiplistMaxLevel:"<<SkiplistMaxLevel<<"\tmax segment size:"<<list->segment_max_size<<"\tDELTA_INSERT:"<<DELTA_INSERT<<std::endl;
@@ -182,23 +224,11 @@ int main(){
     std::cout << "prepare time: " << duration.count() << "us"<<std::endl;
     
     int n = 0;
-    
-    #if TEST_4
-    for(int i = 0;i<16;i++){
-        Insert_Part(n,i);
-    }
-    long long time_sum = 0;
-    for(int i = 0;i<THREAD_NUMBER;i++){
-        time_sum+=insert_time_total[i];
-    }
-    std::cout<<"sum of time:"<<time_sum<<std::endl;
-    #else
-    Insert_Part(n,0);
-    #endif
+    Insert_Part(n,0);  
     #if QUERY_TEST
     n = 0;
     Query_Part(n);
-    #endif   
+    #endif
 
-    std::cout<<"LearnedSkiplist space size:"<<list->SpaceSize() - NUMBERDATA * (sizeof(unsigned int)+ sizeof(int)) <<"\tByte"<<std::endl;
+    std::cout<<"LearnedSkiplist space size:"<<list->SpaceSize() - NUMBERDATA * (sizeof(KeyType)+ sizeof(int)) <<"\tByte"<<std::endl;
 }
